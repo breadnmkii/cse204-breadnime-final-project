d6fb73d0d69e70f9452fef38a076a310285d3dc6
Breadn <bylueang@gmail.com>
2022-12-15 23:13:35 -0600

    js work

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,22 +1,67 @@
 // Functionality for implementing the anime search and interface with
 // the GoGoAnime API
 
+// Note: Use localstorage to cache anime results
 
-// On page load get popular anime
-$(document).ready(() => {
-    console.log("Loading page with data");
-    
-
-})
-
+// Collections of GoGoAnime API functionality for searching anime
+let searchAPI = { 
+    searchAnime: function(searchTerms) {    // Function for redirecting on anime search
+        fetch(`https://gogoanime.consumet.org/search?keyw=${searchTerms}`)
+        .then((response) => response.json())
+        .then((animelist) => {
+            console.log("Searching anime...");
+            localStorage.setItem("searchAnime", JSON.stringify(animelist));
+            window.location.href = "gallery.html";
+        });
+    },
+    openAnime: function(data) {    // Function for redirecting to specific anime JSON object
+        console.log("Opening anime...");
+        localStorage.setItem("openAnime", JSON.stringify(data));
+        window.location.href = "anime.html"
+    },
+    getSearchAnime: function() {    // Function for getting searched anime data
+        data = localStorage.getItem("searchAnime");
+        if (data != null) {
+            data = JSON.parse(data);
+        }
+        return data;
+    },
+    getPopular: function() {
+        data = localStorage.getItem("popularAnime");
+        if (data === null) {
+            fetch("https://gogoanime.consumet.org/popular")
+            .then((response) => response.json())
+            .then((animelist) => {
+                localStorage.setItem("popularAnime", JSON.stringify(animelist));
+            });
+        }
+        return JSON.parse(data);
+    },
+    getRandom: function() {
+        animelist = this.getPopular();
+        if (animelist) {
+            num_anime = Object.keys(animelist).length;
+            console.log(animelist);
+            return animelist[Math.floor(Math.random() * num_anime)];
+        }
+        return undefined;
+    },
+    getAnimeDetails: function(animeID) {
+        fetch(`https://gogoanime.consumet.org/anime-details/${animeID}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            return data;
+        });
+    },
+}
 
 // Search handler
 $('#search').submit(e => {
     e.preventDefault();
-
-
-
+    const searchInput = $('#anime-search').val();
+    searchAPI.searchAnime(searchInput);
     return false;
-})
+});
 
 // Random anime handler
--- static/scripts/view.js
+++ static/scripts/view.js
@@ -1,2 +1,47 @@
 // Functionality for creating dynamic page views
 
+var renderAPI = {
+    renderTrendingCarousel: function(data) {
+        console.log("Rendering trending carousel...");
+        console.log(data);
+        for (let ani in data) {
+            console.log(data[ani]);
+            let currAni = data[ani];
+            $('<div/>', {
+                class: "carousel-item",
+                html: $('<div/>', {
+                    class: "mainview",
+                    html: `                                         \
+                        <div class="img">                           \
+                            <img src="${currAni['animeImg']}">      \
+                        </div>                                      \
+                        <p>${currAni['animeTitle']}</p>             \
+                    `
+                }),
+                click: () => {
+                    alert(`u clicked on ${currAni['animeTitle']}`);
+                    searchAPI.openAnime(currAni);
+                }
+            }).appendTo('.carousel');
+        }
+
+    },
+    renderAnimeGallery: function(data) {
+
+    },
+    renderAnimeStream: function(data) {
+
+    }
+}
+
+
+/*
+<div class="carousel-item">
+    <div class="mainview">
+        <div class="img">
+            <img src="https://i.pinimg.com/originals/4c/3d/14/4c3d14aac7a9ea327d0a080c8c2a1278.jpg">
+        </div>
+        <p>TITLE</p>
+    </div>
+</div>
+*/
