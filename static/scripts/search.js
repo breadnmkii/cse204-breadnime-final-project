e5e88ea0fd06c3a1bb76a196ad8ae8347c4a0dc1
Breadn <bylueang@gmail.com>
2022-12-15 21:24:35 -0600

    outsourced CSS

---
+++ static/scripts/search.js
@@ -0,0 +1,22 @@
+// Functionality for implementing the anime search and interface with
+// the GoGoAnime API
+
+
+// On page load get popular anime
+$(document).ready(() => {
+    console.log("Loading page with data");
+    
+
+})
+
+
+// Search handler
+$('#search').submit(e => {
+    e.preventDefault();
+
+
+
+    return false;
+})
+
+// Random anime handler
