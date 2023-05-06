5afbb978a1e748e1d1e32b3682f33d758712bfa3
breadnmkii <b.z.yang@wustl.edu>
2023-05-06 17:50:28 -0500

    cleanup tsc js

--- app/controllers/view.controllers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Controller endpoint responsible for rendering webpage views
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexView = void 0;
-function indexView() {
-    return { data: "hi" };
-}
-exports.indexView = indexView;
